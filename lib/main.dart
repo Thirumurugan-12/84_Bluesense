62b8ba450c81bc78ad73fb3d22126fadbd2393e4
Thirumurugan-12 <sec22cs123@sairamtap.edu.in>
2024-02-17 23:30:23 +0530

    ui and api integration

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      //darkTheme: ThemeData.dark(),
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -42,17 +43,314 @@ class _MyHomePageState extends State<MyHomePage> {
         length: 2,
         child: Scaffold(
             appBar: AppBar(
-              title: const Text("Plantsense"),
+              title: const Text("PlantSense"),
+              backgroundColor: Colors.greenAccent,
               centerTitle: true,
               bottom: const TabBar(
                 tabs: [
-                  Tab(text: "PlantDOC"),
-                  Tab(text: "Upload Image"),
+                  Tab(text: "PlantDoc"),
+                  Tab(text: "Analyse"),
                 ],
               ),
             ),
             body: const TabBarView(
               children: [TextOnly(), TextWithImage()],
             )));
+  }
+}
+
+// ------------------------------ Text Only ------------------------------
+
+class TextOnly extends StatefulWidget {
+  const TextOnly({
+    super.key,
+  });
+
+  @override
+  State<TextOnly> createState() => _TextOnlyState();
+}
+
+class _TextOnlyState extends State<TextOnly> {
+  bool loading = false;
+  List textChat = [];
+  List textWithImageChat = [];
+
+  final TextEditingController _textController = TextEditingController();
+  final ScrollController _controller = ScrollController();
+
+  // Create Gemini Instance
+  final gemini = GoogleGemini(
+    apiKey: apiKey,
+  );
+
+  // Text only input
+  void fromText({required String query}) {
+    setState(() {
+      loading = true;
+      textChat.add({
+        "role": "User",
+        "text": query,
+      });
+      _textController.clear();
+    });
+    scrollToTheEnd();
+
+    gemini.generateFromText(query).then((value) {
+      setState(() {
+        loading = false;
+        textChat.add({
+          "role": "Gemini",
+          "text": value.text,
+        });
+      });
+      scrollToTheEnd();
+    }).onError((error, stackTrace) {
+      setState(() {
+        loading = false;
+        textChat.add({
+          "role": "Gemini",
+          "text": error.toString(),
+        });
+      });
+      scrollToTheEnd();
+    });
+  }
+
+  void scrollToTheEnd() {
+    _controller.jumpTo(_controller.position.maxScrollExtent);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            controller: _controller,
+            itemCount: textChat.length,
+            padding: const EdgeInsets.only(bottom: 20),
+            itemBuilder: (context, index) {
+              return ListTile(
+                isThreeLine: true,
+                leading: CircleAvatar(
+                  child: Text(textChat[index]["role"].substring(0, 1)),
+                ),
+                title: Text(textChat[index]["role"]),
+                subtitle: Text(textChat[index]["text"]),
+              );
+            },
+          ),
+        ),
+        Container(
+          alignment: Alignment.bottomRight,
+          margin: const EdgeInsets.all(20),
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(color: Colors.grey),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _textController,
+                  decoration: InputDecoration(
+                    hintText: "Type a message",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide.none),
+                    fillColor: Colors.transparent,
+                  ),
+                  maxLines: null,
+                  keyboardType: TextInputType.multiline,
+                ),
+              ),
+              IconButton(
+                icon: loading
+                    ? const CircularProgressIndicator()
+                    : const Icon(Icons.send),
+                onPressed: () {
+                  fromText(query: _textController.text);
+                },
+              ),
+            ],
+          ),
+        )
+      ],
+    ));
+  }
+}
+
+// ------------------------------ Text with Image ------------------------------
+
+class TextWithImage extends StatefulWidget {
+  const TextWithImage({
+    super.key,
+  });
+
+  @override
+  State<TextWithImage> createState() => _TextWithImageState();
+}
+
+class _TextWithImageState extends State<TextWithImage> {
+  bool loading = false;
+  List textAndImageChat = [];
+  List textWithImageChat = [];
+  File? imageFile;
+
+  final ImagePicker picker = ImagePicker();
+
+  final TextEditingController _textController = TextEditingController();
+  final ScrollController _controller = ScrollController();
+
+  // Create Gemini Instance
+  final gemini = GoogleGemini(
+    apiKey: apiKey,
+  );
+  String query = """
+    As a highly skilled plant pathologist, your expertise is indispensable in our pursuit of maintaining optimal plant health. You will be provided with information or samples related to plant diseases, and your role involves conducting a detailed analysis to identify the specific issues, propose solutions, and offer recommendations.
+
+**Analysis Guidelines:**
+
+1. **Disease Identification:** Examine the provided information or samples to identify and characterize plant diseases accurately.
+
+2. **Detailed Findings:** Provide in-depth findings on the nature and extent of the identified plant diseases, including affected plant parts, symptoms, and potential causes.
+
+3. **Next Steps:** Outline the recommended course of action for managing and controlling the identified plant diseases. This may involve treatment options, preventive measures, or further investigations.
+
+4. **Recommendations:** Offer informed recommendations for maintaining plant health, preventing disease spread, and optimizing overall plant well-being.
+
+5. **Important Note:** As a plant pathologist, your insights are vital for informed decision-making in agriculture and plant management. Your response should be thorough, concise, and focused on plant health.
+
+**Disclaimer:**
+*"Please note that the information provided is based on plant pathology analysis and should not replace professional agricultural advice. Consult with qualified agricultural experts before implementing any strategies or treatments."*
+
+Your role is pivotal in ensuring the health and productivity of plants. Proceed to analyze the provided information or samples, adhering to the structured 
+  """;
+  // Text only input
+  void fromTextAndImage({required String query, required File image}) {
+    setState(() {
+      loading = true;
+      textAndImageChat.add({
+        "role": "User",
+        "text": "Analysing",
+        "image": image,
+      });
+      _textController.clear();
+      imageFile = null;
+    });
+    scrollToTheEnd();
+
+    gemini.generateFromTextAndImages(query: query, image: image).then((value) {
+      setState(() {
+        loading = false;
+        textAndImageChat
+            .add({"role": "Gemini", "text": value.text, "image": ""});
+      });
+      scrollToTheEnd();
+    }).onError((error, stackTrace) {
+      setState(() {
+        loading = false;
+        textAndImageChat
+            .add({"role": "Gemini", "text": error.toString(), "image": ""});
+      });
+      scrollToTheEnd();
+    });
+  }
+
+  void scrollToTheEnd() {
+    _controller.jumpTo(_controller.position.maxScrollExtent);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              controller: _controller,
+              itemCount: textAndImageChat.length,
+              padding: const EdgeInsets.only(bottom: 20),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  isThreeLine: true,
+                  leading: CircleAvatar(
+                    child:
+                        Text(textAndImageChat[index]["role"].substring(0, 1)),
+                  ),
+                  title: Text(textAndImageChat[index]["role"]),
+                  subtitle: Text(textAndImageChat[index]["text"]),
+                  trailing: textAndImageChat[index]["image"] == ""
+                      ? null
+                      : Image.file(
+                          textAndImageChat[index]["image"],
+                          width: 90,
+                        ),
+                );
+              },
+            ),
+          ),
+          Container(
+            alignment: Alignment.bottomRight,
+            margin: const EdgeInsets.all(20),
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(color: Colors.grey),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      hintText: "Write a message",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide.none),
+                      fillColor: Colors.transparent,
+                    ),
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline,
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.add_a_photo),
+                  onPressed: () async {
+                    final XFile? image =
+                        await picker.pickImage(source: ImageSource.gallery);
+                    setState(() {
+                      imageFile = image != null ? File(image.path) : null;
+                    });
+                  },
+                ),
+                IconButton(
+                  icon: loading
+                      ? const CircularProgressIndicator()
+                      : const Icon(Icons.send),
+                  onPressed: () {
+                    if (imageFile == null) {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text("Please select an image")));
+                      return;
+                    }
+                    fromTextAndImage(query: query, image: imageFile!);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: imageFile != null
+          ? Container(
+              margin: const EdgeInsets.only(bottom: 80),
+              height: 150,
+              child: Image.file(imageFile ?? File("")),
+            )
+          : null,
+    );
   }
 }
